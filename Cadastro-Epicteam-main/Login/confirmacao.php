5fce7facff0e9c1aa983d390ed2a48e1b57a461f
DiogoYudi <130068159+DiogoYudi@users.noreply.github.com>
2023-06-01 11:54:53 -0300

    Add files via upload

---
+++ Cadastro-Epicteam-main/Login/confirmacao.php
@@ -0,0 +1,16 @@
+<?php
+    require "../Cadastro/bancodedados.php";
+    session_start();
+    $listaClientes = retornarClientes();
+    $nickname = $_POST['fnickname'];
+    $senha = $_POST['fsenha'];
+    while($cliente = mysqli_fetch_assoc($listaClientes)){
+        if ($nickname == $cliente["nickname"] && $senha == $cliente["senha"]){
+            header('Location:../Login/entrou.php');
+            die();
+        }
+    $_SESSION['Resultado'] = "Nickname ou senha incorreta!!";
+    header('Location:../Login/index.php');
+    die();
+    }
+?>
